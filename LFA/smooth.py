eca041141e1065652f49224c89941a17d58542d4
Ruipeng Li <li50@llnl.gov>
2022-10-14 09:36:49 -0700

    initial commit of smoother LFA

---
+++ LFA/smooth.py
@@ -0,0 +1,16 @@
+# import torch
+
+class SmoothSymbl2D:
+    def __int__(self, stencil_m, stencil_a, nu_pre = 1, nu_post = 0):
+        """
+        """
+        self.stencil_m = stencil_m
+        self.stencil_a = stencil_a
+        self.nu_pre = nu_pre
+        self.nu_post = nu_post
+
+    def operator_symbl(self, theta_x, theta_y):
+
+
+    def smoother_symbl(self, theta_x, theta_y):
+
